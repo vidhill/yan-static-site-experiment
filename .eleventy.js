1d35d6779a9fb144ab01130e1641aa28665f5d8d
David Hill <dh366k@us.att.com>
2021-05-25 00:08:26 +0100

    Set up eleventy to serve css files

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
 const markdownShortcode = require('./mardown-shortcode');
 
+const cssPath = "./src/scss/";
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(markdownShortcode);
+    
+    eleventyConfig.addWatchTarget(cssPath);
+    eleventyConfig.addPassthroughCopy(cssPath);
 
     return {
         dir: {
