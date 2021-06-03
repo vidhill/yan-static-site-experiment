7fac5e62c72be49d631189ecf9577aa774d1b3c5
David Hill <dh366k@us.att.com>
2021-06-03 23:52:48 +0100

    Enable eleventy-plugin-svg-contents plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
+const svgContents = require('eleventy-plugin-svg-contents');
 const markdownShortcode = require('./mardown-shortcode');
 
 const cssPath = './src/scss/';
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(markdownShortcode);
+    eleventyConfig.addPlugin(svgContents);
 
     eleventyConfig.addWatchTarget(cssPath);
     eleventyConfig.addPassthroughCopy(cssPath);
