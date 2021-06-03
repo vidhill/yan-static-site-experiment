32bf89c74ce782a38e8ff8ae30ba3e4019fe0b03
David Hill <dh366k@us.att.com>
2021-06-03 23:51:36 +0100

    Prettier auto fix config file

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,17 @@
 const markdownShortcode = require('./mardown-shortcode');
 
-const cssPath = "./src/scss/";
+const cssPath = './src/scss/';
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(markdownShortcode);
-    
+
     eleventyConfig.addWatchTarget(cssPath);
     eleventyConfig.addPassthroughCopy(cssPath);
 
     return {
         dir: {
-            input: "src",
-            output: "public"
-        }
+            input: 'src',
+            output: 'public',
+        },
     };
 };
