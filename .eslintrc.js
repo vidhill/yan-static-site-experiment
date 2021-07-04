4d44e4e815644e0fec936b30efcd069cb3d120ec
David Hill <dh366k@us.att.com>
2021-07-04 15:39:37 +0100

    filenames form 11ty data files need to be camelCase

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,18 @@ module.exports = {
                 node: true,
             },
         },
+        {
+            files: ['src/_data/*'],
+            rules: {
+                // filenames form 11ty data files need to be camelCase to allow for variable not to contain dash for nunjucks
+                // https://github.com/11ty/11ty-website/pull/216
+                'unicorn/filename-case': [
+                    'error',
+                    {
+                        case: 'camelCase',
+                    },
+                ],
+            },
+        },
     ],
 };
