86986dca57d775e7fb3931abc030f01a62a032a4
David Hill <dh366k@us.att.com>
2021-07-04 15:48:47 +0100

    Change env var back to URL

--- src/_data/siteData.js
+++ src/_data/siteData.js
@@ -2,6 +2,6 @@ module.exports = {
     title: 'David Hill Software Engineer',
     subTitle: 'Cork, Ireland',
     username: 'vidhill',
-    absoluteUrl:
-        process.env.DEPLOY_URL || 'http://localhost:' + process.env.PORT,
+    absoluteUrl: process.env.URL || 'http://localhost:' + process.env.PORT,
+    deployUrl: process.env.DEPLOY_URL || '',
 };
